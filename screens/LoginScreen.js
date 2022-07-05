49d8ac0276c38e4e6e64fec249a2be0986df02c8
elfish <41020376+garyfishbough@users.noreply.github.com>
2022-07-04 21:27:08 -0700

    Wk-4 through workshop task-2

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -163,6 +163,21 @@ const RegisterTab = () => {
         setImageUrl(processedImage.uri);
     };
 
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (mediaLibraryPermissions.status === "granted") {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1],
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                processImage(capturedImage.uri);
+            }
+        }
+    };
+
     return (
         <ScrollView>
             <View style={styles.container}>
@@ -173,6 +188,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title='Camera' onPress={getImageFromCamera} />
+                    <Button title='Gallery' onPress={getImageFromGallery} />
                 </View>
                 <Input
                     placeholder='Username'
